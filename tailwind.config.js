b9aac19c743d8e2af9d8ca809a930e27c77392d2
Amin <119117189+am1nNP@users.noreply.github.com>
2023-06-25 16:37:20 +0300

    login  have been made

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js,php}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        vazir:['Vazirmatn']
+      },
+    },
   },
   plugins: [],
 }
